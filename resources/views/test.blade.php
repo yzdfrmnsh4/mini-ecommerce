65224c02b8af7b47d1443a1d1aada371f366f314
Yazid <yaziddev04@gmail.com>
2026-02-01 16:29:41 +0700

    first commit

---
+++ resources/views/test.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>E-commerce</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    
+
+    </head>
+    <body class="bg-slate-400">
+        <div class="bg-red-500 w-screen h-screen">
+
+            <h1 class="text-red-500 font-bold text-2xl bg-black">Hello world</h1>
+        </div>
+    </body>
+</html>
