bb88a6f496b6e4d7e531f1570fc1631dd0067cda
Mochamad Galih Sampoerna <mochtegqr071@gmail.com>
2026-02-05 16:54:02 +0700

    inisialisasi pesanan

--- app/Models/detailTransaksi.php
+++ app/Models/detailTransaksi.php
@@ -3,9 +3,19 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class detailTransaksi extends Model
 {
     protected $table = 'detail_transaksi';
     protected $guarded = ['id'];
+
+
+
+    /**
+     * Get the produk that owns the detailTransaksi
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    // 
 }
